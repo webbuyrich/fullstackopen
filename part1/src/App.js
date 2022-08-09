94fd7b4e62693a415c45ba73ab840d5d9b8845d0
Richard Peterson <webbuyrich@gmail.com>
2022-08-09 09:18:09 -0500

    added simple dynamic content that adds

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,17 @@
 
-/* non shorthand function syntax - add a return */
+/* render dynamic content inside component */
 const App = () => {
+  const now = new Date()
+  const a   = 10
+  const b   = 20
+
   console.log('Hello from component')
   return(
     <div>
-      <p>Hello WOrld</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   )
 }
