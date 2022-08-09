43131cae613515138a3362cadcf3d570cf78c3c1
Richard Peterson <webbuyrich@gmail.com>
2022-08-09 09:33:17 -0500

    added new prop. hardcoded one name. used JS wrapped in curly braces for other values

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,21 +2,22 @@
 const Hello = (props) =>{
   return(
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age}</p>
     </div>
   )
 }
 
 /* render multiple components inside a component */
 const App = () => {
-  
+  const name  = 'Richard'
+  const age   = 42
   return(
     <div>
       <h1>Greeting</h1>
       {/* render the Hello component inside the App component */}
-      <Hello name="Richard"/>
-      <Hello name="Dee"/>
-      <Hello name="N/A"/>
+      <Hello name={name} age={age}/>
+      <Hello name="Dee" age={32+9}/>
+      <Hello name="N/A" age='N/A'/>
     </div>
   )
 }
