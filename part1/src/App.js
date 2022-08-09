88bbb0b34119011318b8cabf80a9611b5ab3c7e4
Richard Peterson <webbuyrich@gmail.com>
2022-08-09 09:41:00 -0500

    added empty brackets to remove extra div

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,18 +7,27 @@ const Hello = (props) =>{
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
 /* render multiple components inside a component */
 const App = () => {
   const name  = 'Richard'
   const age   = 42
   return(
-    <div>
+    <>
       <h1>Greeting</h1>
       {/* render the Hello component inside the App component */}
       <Hello name={name} age={age}/>
       <Hello name="Dee" age={32+9}/>
       <Hello name="N/A" age='N/A'/>
-    </div>
+      <Footer />
+      </>
   )
 }
 
