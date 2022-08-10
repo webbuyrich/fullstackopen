a3d6d32dd291a53239aadb9ad102261d35a379d0
Richard Peterson <webbuyrich@gmail.com>
2022-08-10 11:11:30 -0500

    more statefuluse

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -2,8 +2,10 @@
 import { useState } from "react";
 
 const App = () =>{
+  /* making a funciton call. function useState will return an array with counter and setCounter variables */
   const [ counter, setCounter ] = useState(0)
-
+  /*counter variable is assigned 0 */
+  /*setCounter variable is assigned to a function that will be used to modify the state */
   setTimeout(
     () => setCounter(counter + 1),
     1000
