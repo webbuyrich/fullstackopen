3eb2de2a90fe1c93421fb80f55a6725e259de68c
Richard Peterson <webbuyrich@gmail.com>
2022-08-10 11:49:22 -0500

    defined event handler and removed it from direct JSX

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -4,16 +4,21 @@ import { useState } from "react";
 const App = () => {
 
   const [ counter, setCounter ] = useState(0)
+
+  //Defining event handlers seperate from with JSX
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const setToZero = () => setCounter(0)
   
   return(
     <div>
       <div>{counter}</div>
-      {/*Event Handler function defined directly in value assignemnt */}
-      <button onClick={() => setCounter(counter + 1)}>
+      {/* Event Handler removed from JSX and referenced above in a function */}
+      <button onClick={increaseByOne}>
         plus       
       </button>
-      <button onClick={() => setCounter(0)}>
-        zero       
+      <button onClick={setToZero}>
+        reset       
       </button>
     </div>
      
