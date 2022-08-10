8b75aacceeeaa4b48579dc9aef04a3bd62962329
Richard Peterson <webbuyrich@gmail.com>
2022-08-10 12:14:46 -0500

    added displayand button component. added decrese button function

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -1,28 +1,44 @@
-/* EVENT HANDLING */
+/* PASSING STATE TO CHILD COMPONENTS */
 import { useState } from "react";
 
-const App = () => {
+
+
+const App = () => {  
+
+  // Component to display value of the counter
+const Display = (props) =>{
+  return(
+    <div>{props.counter}</div>
+  )
+}
+
+// Button Component
+const Button = (props) =>{
+  return(
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
 
   const [ counter, setCounter ] = useState(0)
 
   //Defining event handlers seperate from with JSX
   const increaseByOne = () => setCounter(counter + 1)
-
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
   
   return(
     <div>
-      <div>{counter}</div>
+      <Display counter={counter}/>
       {/* Event Handler removed from JSX and referenced above in a function */}
-      <button onClick={increaseByOne}>
-        plus       
-      </button>
-      <button onClick={setToZero}>
-        reset       
-      </button>
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={setToZero} text='zero' />
+      <Button onClick={decreaseByOne} text='minus'/>
+      
     </div>
      
   )
+
+  
 
   
   
