be5cab8141266562973fbab779a660a36f467ad3
Richard Peterson <webbuyrich@gmail.com>
2022-08-10 11:42:38 -0500

    added reset counter event handler with button

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -4,17 +4,22 @@ import { useState } from "react";
 const App = () => {
 
   const [ counter, setCounter ] = useState(0)
-
-
+  
   return(
     <div>
       <div>{counter}</div>
       {/*Event Handler function defined directly in value assignemnt */}
-      <button onClick={() => console.log('clicked')}>
-        plus
+      <button onClick={() => setCounter(counter + 1)}>
+        plus       
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero       
       </button>
     </div>
+     
   )
+
+  
   
 }
 
