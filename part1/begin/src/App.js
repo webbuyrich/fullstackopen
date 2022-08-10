77da6854130aabf7784c3dc4384b9c4756e58216
Richard Peterson <webbuyrich@gmail.com>
2022-08-10 13:29:21 -0500

    added an allClicks array to display L or R to an array on the page

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -9,15 +9,24 @@ const App = () => {
     left: 0, right: 0
   })
 
+  /* Create allClicks array to remember every click */
+  const [allClicks, setAll] = useState([])
+
   /* create new object using ... then set the left and right click property values */
 
   /* refactored event handlers to use setClicks object updates*/
-  const handleLeftClick = () => 
+  const handleLeftClick = () => {
     setClicks({ ...clicks, left: clicks.left + 1 })
+    setAll(allClicks.concat('L'))
+  }
+    
   
 
-  const handleRightClick = () => 
+  const handleRightClick = () => {
     setClicks({ ...clicks, right: clicks.right + 1 })
+    setAll(allClicks.concat('R'))
+  }
+    
   
 
   return(
@@ -26,6 +35,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {clicks.right}
+      <p>{allClicks.join(' - ')}</p>
     </div>
   )
   
