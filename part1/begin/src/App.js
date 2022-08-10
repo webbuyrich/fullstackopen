71579071607a8d2e7c4527e13aeea51345e0d40d
Richard Peterson <webbuyrich@gmail.com>
2022-08-10 11:28:51 -0500

    event handlerdefined directly in value assignment

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -5,14 +5,12 @@ const App = () => {
 
   const [ counter, setCounter ] = useState(0)
 
-  const handleClick = () =>{
-    console.log('clicked')
-  }
 
   return(
     <div>
       <div>{counter}</div>
-      <button onClick={handleClick}>
+      {/*Event Handler function defined directly in value assignemnt */}
+      <button onClick={() => console.log('clicked')}>
         plus
       </button>
     </div>
