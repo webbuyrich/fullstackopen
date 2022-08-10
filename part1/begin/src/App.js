94522b41eaca76e4ea4968c247d2b230dcd1ebb3
Richard Peterson <webbuyrich@gmail.com>
2022-08-10 12:27:28 -0500

    using multiple states with button clicks

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -1,38 +1,25 @@
-/* PASSING STATE TO CHILD COMPONENTS */
+/* USING MUTLIPLE STATES */
 import { useState } from "react";
 
 
 
 const App = () => {  
-
-  // Component to display value of the counter
-const Display = ({ counter }) => <div>{counter}</div>
-
-// Button Component
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
-  const [ counter, setCounter ] = useState(0)
-
-  //Defining event handlers seperate from with JSX
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
   
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
   return(
     <div>
-      <Display counter={counter}/>
-      {/* Event Handler removed from JSX and referenced above in a function */}
-      <Button onClick={increaseByOne} text='plus'/>
-      <Button onClick={setToZero} text='zero' />
-      <Button onClick={decreaseByOne} text='minus'/>
-      
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
-     
   )
-
-  
-
-  
   
 }
 
