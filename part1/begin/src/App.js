85275fee85680a1fffebea75933e25c5829178af
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 08:52:32 -0500

    placing all arrow funcitons on same line

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -6,13 +6,11 @@ import { useState } from 'react'
 const App = () =>{
     const [value, setValue] = useState(10)
 
-    const hello = (who) => {
-        return () => {
+    const hello = (who) => () => {
             console.log('hello', who)
         }         
         
-    }
-
+    
     return(
         <div>
             {value}
