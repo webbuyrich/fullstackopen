04c55a8b2bd81d14226002052deb00fc25d04a1b
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 11:55:01 -0500

    1.8 unicafe step 3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,7 +9,32 @@ const Button = (props) => <button onClick={props.handleClick}>{props.text}</butt
 const StatsHeading = (props) => <h2>{props.text}</h2>
 
 //value displays
-const Statistics = (props) => <div>{props.text} {props.value}</div>
+const Statistic = (props) =>{
+  return(
+    <div>{props.text} {props.value}</div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad, all, average, positive}) => {
+  // test if all prop has a value
+  if(all === 0){
+    // return message if true
+    return(       
+      <div>No feedback given</div>        
+    )} else{
+      // display data 
+      return(
+        <div>
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral}/>
+            <Statistic text="bad" value={bad}/>
+            <Statistic text="all" value={all} />
+            <Statistic text="average" value={average} />
+            <Statistic text="positive" value={positive} />
+          </div>        
+      )
+    }
+}
 
 
 const App = () => {
@@ -28,8 +53,6 @@ const App = () => {
     setBad(newValue)
   }
 
-
-
   return (
     <div>
       <MainHeading text="give feedback"/>
@@ -37,12 +60,14 @@ const App = () => {
       <Button handleClick={()=> setToNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={()=> setToBad(bad + 1)} text="bad" />
       <StatsHeading text="statistics" />
-      <Statistics text="good" value={good}/>
-      <Statistics text="neutral" value={neutral}/>
-      <Statistics text="bad" value={bad}/>
-      <Statistics text="all" value={good + neutral + bad} />
-      <Statistics text="average" value={(good - bad) / (good + neutral + bad)} />
-      <Statistics text="positive" value={( good / (good + neutral + bad) * 100 )  + " %"} />
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={good + neutral + bad}
+        average={(good - bad) / (good + neutral + bad)}
+        positive={( good / (good + neutral + bad) * 100 )  + " %"}
+      />
 
     </div>
   )
