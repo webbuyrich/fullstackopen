db7a7cdbb627644d437ac6c87990fa0a456538c1
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 08:32:24 -0500

    set handleClick to reset value and send message to console.

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -6,10 +6,15 @@ import { useState } from 'react'
 const App = () =>{
     const [value, setValue] = useState(10)
 
+    const handleClick = () => {
+        console.log('clicked button')
+        setValue(0)
+    }
+
     return(
         <div>
             {value}
-            <button onClick={() => setValue(0)}>reset to zero</button>
+            <button onClick={handleClick}>reset to zero</button>
         </div>
     )
 }
