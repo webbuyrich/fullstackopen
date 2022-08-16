a0b57a613c2a4375710c551d08b9399975049acd
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 09:07:55 -0500

    removing arrow funciton from const to onClick

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -6,7 +6,7 @@ import { useState } from 'react'
 const App = () =>{
     const [value, setValue] = useState(10)
 
-    const setToValue = (newValue) => () => {
+    const setToValue = (newValue) => {
         console.log('value now', newValue) // this is the return and it prints the new value to console
         setValue(newValue)
     }        
@@ -15,9 +15,9 @@ const App = () =>{
     return(
         <div>
             {value}
-            <button onClick={setToValue(1000)}>thousand</button>
-            <button onClick={setToValue(0)}>reset</button>
-            <button onClick={setToValue(value + 1)}>increment</button>
+            <button onClick={() => setToValue(1000)}>thousand</button>
+            <button onClick={() => setToValue(0)}>reset</button>
+            <button onClick={() => setToValue(value + 1)}>increment</button>
         </div>
     )
 }
