0dbdd6b27fa3a9a988989bfe9d3f330e412ad1a0
Richard Peterson <webbuyrich@gmail.com>
2022-08-16 08:48:48 -0500

    utilizing functions to return other functions

--- part1/begin/src/App.js
+++ part1/begin/src/App.js
@@ -6,15 +6,19 @@ import { useState } from 'react'
 const App = () =>{
     const [value, setValue] = useState(10)
 
-    const hello = () => {
-        const handler = () => console.log('hello world')
+    const hello = (who) => {
+        const handler = () => {
+            console.log('hello', who)
+        }
         return handler
     }
 
     return(
         <div>
             {value}
-            <button onClick={hello()}>button</button>
+            <button onClick={hello('world')}>button</button>
+            <button onClick={hello('react')}>button</button>
+            <button onClick={hello('function')}>button</button>
         </div>
     )
 }
